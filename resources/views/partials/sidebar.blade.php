fef4cf0ac44a5b40151ab3ddd39918064e76d2e3
zerg84n <zerg84n@gmail.com>
2017-06-28 11:39:23 +0500

    fix en lang

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -140,7 +140,7 @@
             <li class="{{ $request->segment(1) == 'change_password' ? 'active' : '' }}">
                 <a href="{{ route('auth.change_password') }}">
                     <i class="fa fa-key"></i>
-                    <span class="title">Change password</span>
+                    <span class="title">Сменить пароль</span>
                 </a>
             </li>
 
